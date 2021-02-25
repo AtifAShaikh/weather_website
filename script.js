baec8c8464a44fbac5ba6af10ad139280a01ad43
AtifAShaikh <atif.arifshaikh25@gmail.com>
2021-02-24 22:42:03 -0800

    added functionality to view basic weather and conditions and store the citys in local storage for repopulation

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 $('#failedSearchAlert').hide();
 
-function createCard(data){
-    console.log(data);
+var weatherList = localStorage.getItem('weatherList');
+if(weatherList === null){
+    localStorage.setItem('weatherList', '[]');
+    weatherList = JSON.parse(localStorage.getItem('weatherList'));
+}
+weatherList = JSON.parse(localStorage.getItem('weatherList'));
+console.log(weatherList);
+
+function addCityToWeatherList(cityToAdd){
+    if(!weatherList.includes(cityToAdd) && cityToAdd != ''){
+        weatherList.push(cityToAdd);
+        localStorage.setItem('weatherList', JSON.stringify(weatherList));
+        weatherList = JSON.parse(localStorage.getItem('weatherList'));
+    }
     
+}
+
+function createCard(dataInput){
+    // console.log(dataInput);
+    var newCard = $('#templateCard').clone();
+    $(newCard[0]).removeAttr('id');
+    newCard.find('.cityText').text(dataInput.name);
+    newCard.find('.weatherConditionText').text(dataInput.weather[0].main);
+    newCard.find('.tempText').text(Math.round((((dataInput.main.temp-273.15)*1.8)+32)) + " \xB0" + "F");
+    newCard.find('.humidText').text('Humidity: ' + dataInput.main.humidity + "%");
+    newCard.find('.windText').text("Wind: " + (Math.round(dataInput.wind.speed*2.24)) + " mph");
+
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+dataInput.coord.lat+'&lon='+dataInput.coord.lon+'&exclude=hourly,minutely,alerts&appid=891f7c5d1e00dfea698d456d630ff966')
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        // console.log(data);
+    });
+
+    // console.log(newCard);
+    $('.weatherCardContainer').append(newCard);
 }
 
 function getOneCallData(lat, lon){
@@ -10,7 +43,7 @@ function getOneCallData(lat, lon){
     .then(function(response){
         return response.json();
     }).then(function(data){
-        createCard(data);
+        return data.current.uvi;
     });
 }
 
@@ -25,7 +58,9 @@ function getWeatherData(cityToSearch){
             $('#failedSearchAlert').show();
         } else {
             $('#searchModal').modal('toggle');
-            getOneCallData(data.coord.lat, data.coord.lon);
+            // getOneCallData(data.coord.lat, data.coord.lon);
+            addCityToWeatherList($('#cityInput').val());
+            createCard(data);
         }
     });
 }
@@ -34,3 +69,22 @@ $('#searchSubmit').on('click', function(event){
     event.preventDefault();
     getWeatherData($('#cityInput').val());
 });
+
+function populateCards(){
+    weatherList.forEach(element => {
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+ element +"&appid=891f7c5d1e00dfea698d456d630ff966")
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        var test = data.cod;
+        if(test === "404"){
+
+        } else {
+            createCard(data);
+        }
+    });
+    });
+}
+
+populateCards();
